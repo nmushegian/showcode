88dd833831f098e9a66ffa819ac3952141a6b349
nikolai <mail@nikolai.fyi>
2022-04-13 09:34:25 -0400

    harness

--- show.js
+++ show.js
@@ -2,21 +2,11 @@
 _ = PureState
 see =s=> JSON.stringify(s, null, 2)
 
-make =(props)=> {
-    let ent
-    ent = {
-        view: _(()=> see(props()))
-    }
-    ents.push(ent)
-    return ent
-}
 setup =()=> {
     createCanvas(1280, 960)
 
     fill('white')
     stroke('white')
-
-    ents = []
 
     frame = _(0)
     t = _(0)
@@ -25,12 +15,6 @@ setup =()=> {
     cyc = _(cos(0))
     mx = _(mouseX)
     my = _(mouseY)
-    metatext = _()
-
-    s1 = createSlider(0, 100)
-    v1 = _()
-
-    e1 = make(_(() => ({'test':'test'})))
 }
 
 draw =()=> {
@@ -39,25 +23,8 @@ draw =()=> {
     t(t() + deltaTime)
     syc(sin(t()))
     cyc(cos(t()))
-    v1(s1.value())
     mx(mouseX)
     my(mouseY)
-    metatext(`
-frame ${frame()}
-time  ${t()}
-syc   ${syc()}
-cyc   ${cyc()}
-v1    ${v1()}
-mx    ${mx()}
-my    ${my()}
-`)
 
-
-    text(metatext(), 25, 25)
-    for (let ent of ents) {
-        console.log(ent)
-        console.log(ent.view())
-        text(ent.view(), 100, 100)
-    }
 }
 
