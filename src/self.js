d35ce6144e3c49b9c6a484becddf82c5c536bb95
nikolai <mail@nikolai.fyi>
2022-04-21 19:11:31 -0400

    clean

---
+++ src/self.js
@@ -0,0 +1,19 @@
+console.log('load self.js')
+
+const { form, $ } = require('./ents')
+
+export const self = form({
+    x: _.mx,
+    y: _.my,
+    buff: _.sh,
+    hue: $(()=>color(255*_.syc(), 255*_.cyc(), 255-(_.syc())))
+}).tick(() => {
+}).draw(() => {
+    push()
+    fill(self.hue())
+    stroke(self.hue())
+    text("\u2609 ", self.x()+32, self.y())
+    text(self.buff(), self.x()+64, self.y())
+    pop()
+})
+
